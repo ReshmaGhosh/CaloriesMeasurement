340d2966a8d7b986bbb81c539c90fbdf02e2652c
ReshmaGhosh <reshmaghosh114@gmail.com>
2022-12-19 23:34:09 +0100

    Initial Commit

---
+++ CaloriesMeasurement/CaloriesMeasurementApp.swift
@@ -0,0 +1,17 @@
+//
+//  CaloriesMeasurementApp.swift
+//  CaloriesMeasurement
+//
+//  Created by Reshma Ghosh on 2022-12-19.
+//
+
+import SwiftUI
+
+@main
+struct CaloriesMeasurementApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
